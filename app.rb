424e2e20df4c1d2438d591ff3e340a3fc7c74ba2
Orta Therox <orta.therox@gmail.com>
2014-03-01 14:27:02 -0500

    add support for reparsing and running doc with a podspec path

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ class CocoaDocs < Object
 
   # Constrain all downloads and data into one subfolder
   $active_folder_name = "activity"
-  $current_dir = File.dirname(File.expand_path(__FILE__)) 
+  $current_dir = File.dirname(File.expand_path(__FILE__))
   $active_folder = $current_dir + "/" + $active_folder_name
 
   # command line parsing
@@ -58,7 +58,7 @@ class CocoaDocs < Object
 
     if ARGV.length > 0
       setup_options ARGV
-    
+
       command = ARGV[0].gsub(/-/, '_').to_sym rescue :help
       @params = ARGV[1..-1]
       commands.include?(command.to_sym) ? send(command.to_sym) : help
@@ -76,26 +76,26 @@ class CocoaDocs < Object
     Dir.foreach filepath do |pod|
       next if pod[0] == '.'
       next unless File.directory? "#{filepath}/#{pod}/"
-      
+
       Dir.foreach filepath + "/#{pod}" do |version|
         next if version[0] == '.'
         next unless File.directory? "#{filepath}/#{pod}/#{version}/"
-        
+
         document_spec_at_path("#{filepath}/#{pod}/#{version}/#{pod}.podspec")
-        
+
       end
     end
   end
-  
+
   #    start webhook server for incremental building
   #    cocoadocs webhook "CocoaPods/Specs" --create-website http://cocoadocs.org --upload-s3 cocoadocs.org
-  
+
   def webhook
     $upload_docsets_to_s3 = true
     $upload_redirects_for_spec_index = true
     $upload_redirects_for_docsets = true
     $upload_site_to_s3 = true
-    
+
     $generate_website = true
     $generate_docset_json = true
     $generate_apple_json = true
@@ -108,22 +108,30 @@ class CocoaDocs < Object
 
   #    just parse ARAnalytics and put the docset in the activity folder
   #    cocoadocs doc "ARAnalytics"
-  
+
   def doc
-    name = @params[0]
-    spec_path = $active_folder + "/#{$cocoadocs_specs_name}/" + name
     update_specs_repo
-    
-    
-    if Dir.exists? spec_path
-      version = Dir.entries(spec_path).last
-      document_spec_at_path("#{spec_path}/#{version}/#{name}.podspec")
-      Process.exit
+
+    name = @params[0]
+    spec_path = $active_folder + "/#{$cocoadocs_specs_name}/"
+
+    if name.include? ".podspec"
+      spec_path = name
+    else
+      if Dir.exists? spec_path  + name
+        version = Dir.entries(spec_path + name).last
+        spec_path = "#{spec_path + name}/#{version}/#{name}.podspec"
+      end
+    end
+
+    if spec_path.include? ".podspec"
+      document_spec_at_path spec_path
     else
       puts "Could not find #{name} at #{spec_path}"
     end
   end
-  
+
+
   def cocoadocs
     if @params[0] == "doc"
       cocoadocs_doc
@@ -131,14 +139,14 @@ class CocoaDocs < Object
       cocoadocs_day
     end
   end
-  
+
   def setup_for_cocoadocs
-    
+
     $generate_website = true
     $generate_docset_json = true
     $generate_apple_json = true
     $website_home = "http://cocoadocs.org/"
-  
+
     $upload_docsets_to_s3 = true
     $upload_redirects_for_spec_index = true
     $upload_redirects_for_docsets = true
@@ -146,21 +154,21 @@ class CocoaDocs < Object
     $s3_bucket = "cocoadocs.org"
 
   end
-  
+
   def cocoadocs_day
     setup_for_cocoadocs
-    
+
     updated_specs = specs_for_days_ago_diff @params[1]
     vputs "Looking at #{updated_specs.lines.count}"
-    
+
     updated_specs.lines.each_with_index do |spec_filepath, index|
       spec_path = $active_folder + "/" + $cocoadocs_specs_name + "/" + spec_filepath.strip
       next unless spec_filepath.include? ".podspec" and File.exists? spec_path
-    
-      document_spec_at_path spec_path  
+
+      document_spec_at_path spec_path
     end
   end
-  
+
   def cocoadocs_doc
     setup_for_cocoadocs
     @params[0] = @params[1]
@@ -168,13 +176,13 @@ class CocoaDocs < Object
   end
 
   def help
-    puts "\n" +                                                                  
+    puts "\n" +
     "    CocoaDocs command line                                                    \n" +
     "                                                                              \n" +
     "     app.rb all                                                               \n" +
     "     app.rb webhook                                                           \n" +
-    "     app.rb doc [spec]                                                        \n" +
-    "     app.rb cocoadocs doc [spec]                                              \n" +
+    "     app.rb doc [spec name or podspec path]                                   \n" +
+    "     app.rb cocoadocs doc [spec name]                                         \n" +
     "     app.rb cocoadocs days [days]                                             \n" +
     "                                                                              \n" +
     "     Options:                                                                 \n" +
@@ -198,7 +206,7 @@ class CocoaDocs < Object
     "      just parse ARAnalytics and put the docset in the activity folder        \n" +
     "      app.rb doc ARAnalytics                                                  \n\n"
   end
-  
+
   # Take a webhook, look at the commits inbetween the before & after
   # and then document each spec.
 
@@ -206,28 +214,28 @@ class CocoaDocs < Object
     before = webhook_payload["before"]
     after = webhook_payload["after"]
     vputs "Got a webhook notification for #{before} to #{after}"
-  
+
     update_specs_repo
     updated_specs = specs_for_git_diff before, after
-    vputs "Looking at #{updated_specs.lines.count}"
-  
+    vputs "Looking at #{ updated_specs.lines.count }"
+
     updated_specs.lines.each_with_index do |spec_filepath, index|
       spec_path = $active_folder + "/" + $cocoadocs_specs_name + "/" + spec_filepath.strip
       next unless spec_filepath.include? ".podspec" and File.exists? spec_path
-    
-      document_spec_at_path spec_path  
+
+      document_spec_at_path spec_path
     end
   end
-  
+
   private
 
   def setup_options options
-    
+
     if options.find_index("--verbose") != nil
       $verbose = true
       $log_all_terminal_commands = true
     end
-    
+
     if options.find_index("--skip-fetch") != nil
       $fetch_specs = false
     end
@@ -244,7 +252,7 @@ class CocoaDocs < Object
       $generate_apple_json = true
       $website_home = options[index + 1]
     end
-  
+
     index = options.find_index("--upload-s3")
     if index != nil
       $upload_docsets_to_s3 = true
@@ -254,12 +262,12 @@ class CocoaDocs < Object
       $s3_bucket = options[index + 1]
     end
 
-    index = options.find_index "--specs-repo"    
+    index = options.find_index "--specs-repo"
     $specs_repo = options[index + 1] if index != nil
-    
-    index = options.find_index "--data-folder"    
+
+    index = options.find_index "--data-folder"
     $active_folder_name = options[index + 1] if index != nil
-    
+
     $active_folder = $current_dir + "/" + $active_folder_name
   end
 
@@ -279,10 +287,10 @@ class CocoaDocs < Object
         run_git_command_in_specs "stash"
         run_git_command_in_specs "pull origin master"
       end
-    end  
+    end
   end
 
-  
+
   # returns an array from the diff log for the last x days
   def specs_for_days_ago_diff days_ago
     sha = run_git_command_in_specs 'rev-list -n1 --before="' + days_ago + ' day ago" master'
@@ -301,10 +309,10 @@ class CocoaDocs < Object
   # cleans up and removes modification notice to the diff
   def cleanup_git_logs diff_log
     diff_log.lines.map do |line|
-    
+
       line.slice!(0).strip!
       line.gsub! /\t/, ''
-    
+
     end.join
   end
 
@@ -315,63 +323,63 @@ class CocoaDocs < Object
      `git #{git_command}`
     end
   end
-  
+
   # generate the documentation for the pod
 
   def document_spec_at_path spec_path
     spec = nil
-    begin 
+    begin
       spec = eval(File.open(spec_path).read)
 
       download_location = $active_folder + "/download/#{spec.name}/#{spec.version}/#{spec.name}"
       docset_location   = $active_folder + "/docsets/#{spec.name}/#{spec.version}/"
       readme_location   = $active_folder + "/readme/#{spec.name}/#{spec.version}/index.html"
       pod_root_location = $active_folder + "/docsets/#{spec.name}/"
-      
+
       if $run_docset_commands
-    
+
         downloader = SourceDownloader.new ({ :spec => spec, :download_location => download_location, :overwrite => $overwrite_existing_source_files })
         downloader.download_pod_source_files
-    
+
         readme = ReadmeGenerator.new ({ :spec => spec, :readme_location => readme_location })
         readme.create_readme
 
         generator = DocsetGenerator.new({ :spec => spec, :to => docset_location, :from => download_location, :readme_location => readme_location })
         generator.create_docset
-    
+
         fixer = DocsetFixer.new({ :docset_path => docset_location, :readme_path => readme_location, :pod_root => pod_root_location, :spec => spec })
         fixer.fix
         fixer.add_index_redirect_to_latest_to_pod if $upload_redirects_for_spec_index
         fixer.add_docset_redirects if $upload_redirects_for_docsets
-    
+
         spec_metadata = SpecMetadataGenerator.new({ :spec => spec })
         spec_metadata.generate
-    
+
       end
-  
+
  #     $parser = AppleJSONParser.new
  #     $parser.generate if $generate_apple_json
 
       $generator = WebsiteGenerator.new(:generate_json => $generate_docset_json, :spec => spec)
       $generator.upload_docset if $upload_docsets_to_s3
-      
+
       $generator.generate if $generate_website
       $generator.upload_site if $upload_site_to_s3
-      
-      if $delete_source_after_docset_creation       
+
+      if $delete_source_after_docset_creation
         vputs "Deleting source files"
-        command "rm -rf #{download_location}" 
-        command "rm -rf #{docset_location}" 
+        command "rm -rf #{download_location}"
+        command "rm -rf #{docset_location}"
       end
-    end 
-  
+    end
+
   rescue Exception => e
     if spec != nil
       error_path = "errors/#{spec.name}/#{spec.version}/error.json"
       FileUtils.mkdir_p(File.dirname(error_path))
       FileUtils.rm(error_path) if File.exists? error_path
-     
-      open(error_path, 'a'){ |f| 
+
+      open(error_path, 'a'){ |f|
         report = { "message" => e.message , "trace" => e.backtrace }
         f.puts report.to_json.to_s
       }
@@ -388,7 +396,7 @@ class CocoaDocs < Object
     puts e.message.red
     puts "------"
     puts e.backtrace.inspect.red
-  
+
   end
 
   def commands
@@ -404,11 +412,11 @@ docs = CocoaDocs.new(ARGV)
 
 if $start_sinatra_server
   require 'sinatra'
-  
+
   post "/webhook" do
     docs.handle_webhook JSON.parse(params[:payload])
   end
-  
+
   get "/error/:pod/:version" do
     # get error info for a pod
      error_json_path = "errors/#{params[:pod]}/#{params[:version]}/error.json"
@@ -417,5 +425,21 @@ if $start_sinatra_server
      end
      return "{}"
   end
-  
+
+  get "/redeploy/:pod/:version" do
+
+    repo_path = $active_folder + "/#{$cocoadocs_specs_name}/"
+    podspec_path = repo_path + "/#{params[:pod]}/#{params[:version]}/#{params[:pod]}.podspec"
+
+     if File.exists? podspec_path
+
+        pid = Process.spawn("ruby", "app.rb", "doc", podspec_path, :out => 'dev/null', :err => 'dev/null')
+        Process.detach pid
+
+       return "{ parsing: true }"
+     end
+
+     return "{ parsing: false }"
+  end
+
 end
