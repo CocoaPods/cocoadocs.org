db6b1952c2efd0179a74cc1de333ba53312a948b
Orta Therox <orta.therox@gmail.com>
2014-05-08 09:45:47 +0100

    use twitter svg and remove @ sign on social media urls part of #147

--- classes/spec_extensions.rb
+++ classes/spec_extensions.rb
@@ -22,40 +22,48 @@ module Pod
       def or_extensionless_homepage
         return nil unless self.homepage
         self.homepage.sub('http://', '').sub('https://', '').sub('www.', '').split("/")[0]
-      end      
+      end
 
-      def or_contributors_to_spec 
+      def or_contributors_to_spec
         return self.authors if self.authors.is_a? String
         return self.authors.listify if self.authors.is_a? Array
         return self.authors.keys.listify if self.authors.is_a? Hash
       end
 
-      def or_license 
+      def or_license
         return self.license if self.license.is_a? String
         return self.license[:type] if self.license.is_a? Hash
         return "Unknown License"
       end
-      
+
       def or_has_social_media_url?
         self.social_media_url != nil
       end
-      
+
+      def or_has_twitter_url?
+        self.social_media_url.include?("twitter.com")
+      end
+
+      def or_twitter_handle
+        return self.social_media_url.split(".com/")[-1]
+      end
+
       def or_social_media_title
-        if self.social_media_url.include?("twitter.com") 
+        if self.social_media_url.include?("twitter.com")
           return "@" + self.social_media_url.split(".com/")[-1]
         end
-        
-        if self.social_media_url.include?("facebook.com") 
+
+        if self.social_media_url.include?("facebook.com")
           return "FB: " + self.social_media_url.split(".com/")[-1]
         end
-        
-        if self.social_media_url.include?("github.com") 
+
+        if self.social_media_url.include?("github.com")
           return "GH: " + self.social_media_url.split(".com/")[-1]
         end
-        
+
         self.social_media_url
       end
-      
+
       def or_spec_is_deprecated?
         self.deprecated || self.deprecated_in_favor_of
       end
