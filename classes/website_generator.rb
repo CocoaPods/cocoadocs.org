e01abc205cafa0dcf951a0fe403cfb2731cc6dd1
Orta <orta.therox@gmail.com>
2014-05-24 15:13:45 -0400

    remove more stuff

--- classes/website_generator.rb
+++ classes/website_generator.rb
@@ -5,25 +5,8 @@ class WebsiteGenerator
   attr_accessor :generate_json, :spec
 
   def generate
-    create_index_page
     move_public_items
     create_stylesheet
-  end
-
-  def create_index_page
-    vputs "Creating index page"
-
-    save_slim "views/index.slim", "#{$active_folder}/html/index.html"
-    save_slim "views/404.slim", "#{$active_folder}/html/404.html"
-  end
-
-
-  def save_slim slim_filepath, to_filepath
-    template = Tilt.new slim_filepath
-    html = template.render
-
-    vputs "Writing slim_filepath"
-    seve_file html, to_filepath
   end
 
   def create_stylesheet
@@ -31,25 +14,6 @@ class WebsiteGenerator
     command "sass views/homepage_stylesheet.scss:#{$active_folder}/html/assets/homepage_stylesheet.css"
     command "sass views/appledoc_stylesheet.scss:#{$active_folder}/html/assets/appledoc_stylesheet.css"
     command "sass views/appledoc_gfm.scss:#{$active_folder}/html/assets/appledoc_gfm.css"
-  end
-
-  def create_specs_json specs
-    array_string = specs.to_json.to_s
-
-    function_wrapped = "var specs = #{array_string}; searchTermChanged()"
-    json_filepath = $active_folder + "/html/documents.jsonp"
-
-    vputs "Writing JSON for docsets"
-    seve_file function_wrapped, json_filepath
-  end
-
-  def seve_file file, path
-    FileUtils.mkdir_p(File.dirname(path))
-    if File.exists? path
-       File.unlink path
-    end
-
-    File.open(path, "wb") { |f| f.write file }
   end
 
   def move_public_items
@@ -73,6 +37,22 @@ class WebsiteGenerator
     upload_folder "html/*", "/", "put"
   end
 
+  def save_file file, path
+    FileUtils.mkdir_p(File.dirname(path))
+    File.unlink path if File.exists? path
+       
+    File.open(path, "wb") { |f| f.write file }
+  end
+
+
+  def save_slim slim_filepath, to_filepath
+    template = Tilt.new slim_filepath
+    html = template.render
+
+    vputs "Writing slim_filepath"
+    save_file html, to_filepath
+  end
+
 
   # Upload the docsets folder to s3
   def upload_folder from, to, command
@@ -83,7 +63,7 @@ class WebsiteGenerator
       "--recursive  --acl-public",
       "--no-check-md5",
       "--verbose --human-readable-sizes --reduced-redundancy",
-      "#{$active_folder}/#{from} s3://cocoadocs.org#{to}"
+      "#{ $active_folder }/#{from} s3://#{ $s3_bucket }#{to}"
     ]
 
     command upload_command.join(' ')
