355fa4d8b4583d8ea035c56f72658bdb12e93aac
Orta <orta.therox@gmail.com>
2014-05-26 14:19:55 -0400

    try catch the error

--- classes/spec_metadata_generator.rb
+++ classes/spec_metadata_generator.rb
@@ -4,18 +4,22 @@ class SpecMetadataGenerator
 
   def generate
     vputs "Generating the Specs version metadata and all that"
+    begin
+      trunk_spec = Net::HTTP.get(URI("https://trunk.cocoapods.org/api/v1/pods/" + @spec.name))
+      versions = JSON.parse(trunk_spec)["versions"]
+      versions = versions.map { |s| Pod::Version.new(s["name"]) }.sort.map { |semver| semver.version }
 
-    trunk_spec = Net::HTTP.get(URI("https://trunk.cocoapods.org/api/v1/pods/" + @spec.name))
-    versions = JSON.parse(trunk_spec)["versions"]
-    versions = versions.map { |s| Pod::Version.new(s["name"]) }.sort.map { |semver| semver.version }
+      hash_string = {
+        :versions => versions,
+      }.to_json.to_s
 
-    hash_string = {
-      :versions => versions,
-    }.to_json.to_s
+      json_filepath = @docset_path + "../metadata.json"
 
-    json_filepath = @docset_path + "../metadata.json"
+      File.open(json_filepath, "wb") { |f| f.write hash_string }
 
-    File.open(json_filepath, "wb") { |f| f.write hash_string }
+    rescue Exception => e
+      puts "Error generating Spec metadata: " + e
+    end
   end
 
 end
