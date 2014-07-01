91f59ca20a64702774c386cb82d6551dd5c2ecc9
Orta Therox <orta.therox@gmail.com>
2014-07-01 01:09:36 -0400

    website

---
+++ views/assets/search.js
@@ -0,0 +1,85 @@
+$(window).ready(function() {
+
+    var searchInput = $('#search input[type="search"]');
+    var helpText = $('#search fieldset p');
+
+    var platformRemoverRegexp = /\b(platform|on\:\w+\s?)+/;
+
+    var allocationSelect = $('#search_results div.allocations');
+    var resultsContainer = $('#results_container .results');
+
+    var itemClasses = "col-lg-offset-1 col-sm-offset-1 col-xs-offset-1col-lg-5 col-sm-5 col-xs-10 result"
+    var itemClassesOdd = " col-lg-5 col-sm-5 col-xs-10 result"
+
+    searchInput.keyup(function() {
+        var input = $(this).val()
+        var url = 'http://search.cocoapods.org/api/v1/pods.flat.hash.json?query=' + input
+
+        if (!input.length){
+            $('#search form').removeClass("active");
+            resultsContainer.empty("div");
+            return;
+        }
+
+        $.getJSON(encodeURI(url), function(results, textStatus, jqXHR) {
+            resultsContainer.empty("div");
+
+            if (results.length) {
+
+                for (var index in results) {
+                    var result = results[index]
+                    var cd = "http://cocoadocs.org/docsets/"
+                    var latestURL =  cd + result["id"]
+                    var classes = (index % 2) ? itemClassesOdd : itemClasses;
+                    var element = {
+                        li : $("<li>", { class: classes }),
+                        wrapper : $("<div>"),
+                        title : $("<h2>", { text: result["id"] }),
+                        description : $("<p>", { text: result["summary"] }),
+                        linksList : $("<ul>"),
+                        latest : $("<li>"),
+                        latestLink : $("<a>", { text: "Latest Docs", href: latestURL })
+                    };
+
+                    element.latest.append(element.latestLink)
+                    element.linksList.append(element.latest)
+                    element.wrapper.append(element.title, element.description, element.linksList)
+                    element.li.append(element.wrapper)
+                    resultsContainer.append(element.li)
+
+                    // trunk is too slow to do this
+                    // resultsContainer.mouseenter(function() {
+                    //     if ( $(this).hasClass("downloaded") ){
+                    //         return;
+                    //     }
+                    //
+                    //     $(this).addClass("downloaded")
+                    //     var versions_url = "https://trunk.cocoapods.org/api/v1/pods/" +  result["id"]
+                    //     $.getJSON(encodeURI(versions_url), function(results, textStatus, jqXHR) {
+                    //         console.log(results)
+                    //
+                    //         for (var version in results["versions"]) {
+                    //             $( this ).find( "ul" ).append("<li><a href='" + cd + "/" + version["name"] + "'>" + version["name"] + "</a></li>");
+                    //
+                    //         }
+                    //     })
+                    // })
+
+
+                    if (index % 2){
+                       resultsContainer.append( $("<div>", { class: "clearfix" }) )
+                    }
+                }
+                $('#search form').addClass("active");
+
+            } else {
+
+            }
+
+
+        });
+
+    });
+
+
+});
---
+++ webpages.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'tilt'
+require 'slim'
+require 'fileutils'
+require_relative "classes/_utils"
+
+@prefix = File.expand_path File.dirname(__FILE__)
+
+def save_slim slim_filepath, to_filepath
+
+  template = Tilt.new @prefix + "/" + slim_filepath
+  html = template.render
+
+  vputs "Writing slim_filepath"
+  save_file html, @prefix + "/" + to_filepath
+end
+
+def save_file file, path
+  FileUtils.mkdir_p(File.dirname(path))
+  File.unlink path if File.exists? path
+
+  File.open(path, "wb") { |f| f.write file }
+end
+
+def copy_folder from, to
+  command "cp -R #{ @prefix }/#{from}/* #{ @prefix }/#{to}"
+end
+
+
+save_slim "views/index.slim", "activity/website/index.html"
+copy_folder "views/images", "activity/website/images"
+copy_folder "views/assets", "activity/website/assets"
+
+puts "done"
