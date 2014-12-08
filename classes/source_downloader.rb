2c75bb772c5cb48d8937165ca9a7ba29dba8a973
Samuel E. Giddins <segiddins@segiddins.me>
2014-12-08 14:47:13 -0800

    [SourceDownloader] Run prepare_command

--- classes/source_downloader.rb
+++ classes/source_downloader.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/string/strip'
+
 class SourceDownloader
   include HashInit
   attr_accessor :spec, :download_location, :overwrite
@@ -22,5 +24,27 @@ class SourceDownloader
 
     downloader = Pod::Downloader.for_target(@download_location, source)
     downloader.download
+    run_prepare_command
+  end
+
+  # Runs the prepare command bash script of the spec.
+  #
+  # @note   Unsets the `CDPATH` env variable before running the
+  #         shell script to avoid issues with relative paths
+  #         (issue #1694).
+
+  # @note   Copied from CocoaPods/CocoaPods
+  #
+  # @return [void]
+  #
+  def run_prepare_command
+    return unless @spec.root.prepare_command
+    vputs ' > Running prepare command'
+    Dir.chdir(@download_location) do
+      ENV.delete('CDPATH')
+      prepare_command = @spec.root.prepare_command.strip_heredoc.chomp
+      full_command = "\nset -e\n" + prepare_command
+      `#{full_command}`
+    end
   end
 end
