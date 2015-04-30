83541fd619b6262c02084600108824e9523fabda
Orta Therox <orta.therox@gmail.com>
2015-04-30 09:34:36 -0400

    split out the readme manipulation into its own object

--- classes/docset_fixer.rb
+++ classes/docset_fixer.rb
@@ -6,14 +6,15 @@ class DocsetFixer
   include HashInit
   attr_accessor :docset_path, :readme_path, :pod_root, :spec, :css_path, :doc_percent, :versions
 
+  def manipulator
+    ReadmeManipulator.new({ :readme_path => @readme_path, :spec => @spec })
+  end
+
   def fix
     @version = spec.version
     remove_html_folder
     delete_extra_docset_folder
-    fix_relative_links_in_gfm
-    remove_known_badges
-    remove_named_header
-    fix_header_anchors
+    manipulator.run_for_cocoadocs
     move_gfm_readme_in
     move_css_in
     move_docset_icon_in
@@ -25,10 +26,7 @@ class DocsetFixer
 
   def fix_for_jazzy
     @version = spec.version
-    fix_relative_links_in_gfm('article.chapter')
-    remove_known_badges
-    remove_named_header
-    fix_header_anchors
+    manipulator.run_for_jazzy
     create_dash_data
     minify_html
     create_separate_readme('article.chapter')
@@ -101,109 +99,6 @@ class DocsetFixer
   def delete_extra_docset_folder
     vputs "Removing redundant docset extracts"
     command "rm -Rf \"#{@docset_path}\"/docset"
-  end
-
-  def fix_relative_link(link_string)
-    if link_string.start_with? "#"
-      return link_string
-    end
-    if link_string.start_with? "http"
-      return link_string
-    end
-    if link_string.start_with? "https"
-      return link_string
-    end
-    if link_string.include? "@"
-      return link_string
-    end
-
-    "https://raw.github.com/#{@spec.or_user}/#{@spec.or_repo}/#{@spec.or_git_ref}/#{CGI.escape link_string}"
-  end
-
-  def fix_relative_links_in_gfm(parent_selector = nil)
-    vputs "Fixing relative URLs in github flavoured markdown"
-
-    return unless @spec.or_is_github?
-    return unless File.exist? @readme_path
-
-    doc = Nokogiri::HTML(File.read @readme_path)
-    main = parent_selector ? doc.css(parent_selector).first : doc
-    main.css("a").each do |link|
-      if link.attributes["href"]
-        link.attributes["href"].value = fix_relative_link link.attributes["href"].value
-      end
-    end
-
-    main.css("img").each do |img|
-      if img.attributes["src"]
-        img.attributes["src"].value = fix_relative_link img.attributes["src"].value
-      end
-    end
-
-    `rm \"#{@readme_path}\"`
-    File.open(@readme_path, 'w') { |f| f.write(doc) }
-  end
-
-  # Don't have a redundant header under the site's provided one
-  def remove_named_header
-    doc = Nokogiri::HTML(File.read @readme_path)
-    header_anchor = doc.css("body").children[1]
-    header_anchor.remove if header_anchor.text.strip == @spec.name
-
-    `rm \"#{@readme_path}\"`
-    File.open(@readme_path, 'w') { |f| f.write(doc) }
-  end
-
-  def remove_known_badges
-    vputs "Fixing Travis links in markdown"
-
-    return unless @spec.or_is_github?
-    return unless File.exist? @readme_path
-
-    doc = Nokogiri::HTML(File.read @readme_path)
-
-    doc.css('a[href^="https://travis-ci.org"]').each do |link|
-      link.remove if link.inner_html.include? ".svg"
-      link.remove if link.inner_html.include? ".png?branch"
-    end
-
-    urls_to_delete = ['http://cocoapod-badges.herokuapp',
-                      'https://cocoapod-badges.herokuapp',
-                      'https://img.shields.io',
-                      'http://img.shields.io',
-                      'https://reposs.herokuapp.com',
-                      'https://secure.travis-ci.org',
-                      'https://kiwiirc.com',
-                      'https://badges.gitter.im/',
-                      'https://badge.waffle.io']
-    urls_to_delete.each do |selector|
-      doc.css('img[data-canonical-src^="' + selector + '"]').each do |image|
-        image.parent.remove
-      end
-    end
-
-    `rm \"#{@readme_path}\"`
-    File.open(@readme_path, 'w') { |f| f.write(doc) }
-  end
-
-  def fix_header_anchors
-    vputs "Fixing header anchor names"
-
-    return unless @spec.or_is_github?
-    return unless File.exist? @readme_path
-
-    doc = Nokogiri::HTML(File.read @readme_path)
-
-    nodes = doc.css('h1, h2, h3')
-    nodes.each do |node|
-      href = node.css('a').first
-      if href && href.attributes["name"]
-        href.attributes["name"].value = href.attributes["name"].value.gsub(/user-content-/, "")
-      end
-    end
-
-    `rm \"#{@readme_path}\"`
-    File.open(@readme_path, 'w') { |f| f.write(doc) }
   end
 
   def move_docset_icon_in
---
+++ classes/readme_manipulator.rb
@@ -0,0 +1,125 @@
+# Takes a readme path and 
+
+class ReadmeManipulator
+  include HashInit
+  attr_accessor :spec, :readme_path
+
+  def run_for_cocoadocs
+    fix_relative_links_in_gfm
+    remove_known_badges
+    remove_named_header
+    fix_header_anchors
+  end
+  
+  def run_for_jazzy
+    fix_relative_links_in_gfm('article.chapter')
+    remove_known_badges
+    remove_named_header
+    fix_header_anchors
+  end
+  
+  def fix_relative_link(link_string)
+    if link_string.start_with? "#"
+      return link_string
+    end
+    if link_string.start_with? "http"
+      return link_string
+    end
+    if link_string.start_with? "https"
+      return link_string
+    end
+    if link_string.include? "@"
+      return link_string
+    end
+
+    "https://raw.github.com/#{@spec.or_user}/#{@spec.or_repo}/#{@spec.or_git_ref}/#{CGI.escape link_string}"
+  end
+
+  def fix_relative_links_in_gfm(parent_selector = nil)
+    vputs "Fixing relative URLs in github flavoured markdown"
+
+    return unless @spec.or_is_github?
+    return unless File.exist? @readme_path
+
+    doc = Nokogiri::HTML(File.read @readme_path)
+    main = parent_selector ? doc.css(parent_selector).first : doc
+    main.css("a").each do |link|
+      if link.attributes["href"]
+        link.attributes["href"].value = fix_relative_link link.attributes["href"].value
+      end
+    end
+
+    main.css("img").each do |img|
+      if img.attributes["src"]
+        img.attributes["src"].value = fix_relative_link img.attributes["src"].value
+      end
+    end
+
+    `rm \"#{@readme_path}\"`
+    File.open(@readme_path, 'w') { |f| f.write(doc) }
+  end
+
+  # Don't have a redundant header under the site's provided one
+  def remove_named_header
+    doc = Nokogiri::HTML(File.read @readme_path)
+    header_anchor = doc.css("body").children[1]
+    header_anchor.remove if header_anchor.text.strip == @spec.name
+
+    `rm \"#{@readme_path}\"`
+    File.open(@readme_path, 'w') { |f| f.write(doc) }
+  end
+
+  def remove_known_badges
+    vputs "Fixing Travis links in markdown"
+
+    return unless @spec.or_is_github?
+    return unless File.exist? @readme_path
+
+    doc = Nokogiri::HTML(File.read @readme_path)
+
+    doc.css('a[href^="https://travis-ci.org"]').each do |link|
+      link.remove if link.inner_html.include? ".svg"
+      link.remove if link.inner_html.include? ".png?branch"
+    end
+
+    urls_to_delete = ['http://cocoapod-badges.herokuapp',
+                      'https://cocoapod-badges.herokuapp',
+                      'https://img.shields.io',
+                      'http://img.shields.io',
+                      'https://reposs.herokuapp.com',
+                      'https://secure.travis-ci.org',
+                      'https://kiwiirc.com',
+                      'https://badges.gitter.im/',
+                      'https://badge.waffle.io']
+    urls_to_delete.each do |selector|
+      doc.css('img[data-canonical-src^="' + selector + '"]').each do |image|
+        image.parent.remove
+      end
+    end
+
+    `rm \"#{@readme_path}\"`
+    File.open(@readme_path, 'w') { |f| f.write(doc) }
+  end
+
+  def fix_header_anchors
+    vputs "Fixing header anchor names"
+
+    return unless @spec.or_is_github?
+    return unless File.exist? @readme_path
+
+    doc = Nokogiri::HTML(File.read @readme_path)
+
+    nodes = doc.css('h1, h2, h3')
+    nodes.each do |node|
+      href = node.css('a').first
+      if href && href.attributes["name"]
+        href.attributes["name"].value = href.attributes["name"].value.gsub(/user-content-/, "")
+      end
+    end
+
+    `rm \"#{@readme_path}\"`
+    File.open(@readme_path, 'w') { |f| f.write(doc) }
+  end
+
+
+end
