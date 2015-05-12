b1f928d9ddddb6004ffdd11964b5a51e0315183e
Samuel E. Giddins <segiddins@segiddins.me>
2015-05-12 13:13:28 -0700

    [Spec] Use the correct case for the Alamofire podspec fixture

--- spec/readme_manipulator_spec.rb
+++ spec/readme_manipulator_spec.rb
@@ -12,7 +12,7 @@ end
 def rendered_readme_for pod
   tmp_readme = "/tmp/#{pod}.html"
   `cp spec/fixtures/readme/#{pod}.html #{tmp_readme}`
-  
+
   spec = Pod::Specification.from_file "spec/fixtures/podspecs/#{pod}.podspec.json"
   manipulator = ReadmeManipulator.new( :readme_path => tmp_readme, :spec => spec )
   manipulator.run_for_cocoadocs
@@ -20,27 +20,27 @@ def rendered_readme_for pod
 end
 
 describe 'with CDZUIKitAutoLayoutDebugging' do
-  
+
   before do
     @readme = rendered_readme_for "CDZUIKitAutoLayoutDebugging"
   end
 
-  it 'should remove all badges ' do    
+  it 'should remove all badges ' do
     @readme.should.not contain('shields.io')
   end
 
   it 'should remove the main header ' do
     @readme.should.not contain('CDZUIKitAutoLayoutDebugging</h1>')
   end
-  
+
   it 'should not feature carthage' do
     @readme.should.not contain('dependency to your Cartfile')
   end
-  
+
   it 'should not feature cocoapods' do
     @readme.should.not contain("pod 'CDZUIKitAutoLayoutDebugging'")
   end
-  
+
 end
 
 describe 'with Cent' do
@@ -50,22 +50,22 @@ describe 'with Cent' do
 end
 
 describe 'with HanekeSwift' do
-  it 'should not mention carthage' do    
+  it 'should not mention carthage' do
      rendered_readme_for("HanekeSwift").should.not contain('Using Carthage')
   end
-  
+
   it 'should not feature CocoaPods' do
     rendered_readme_for("HanekeSwift").should.not contain("pod 'Hanake")
   end
 end
 
-describe 'with AlamoFire' do
+describe 'with Alamofire' do
   it 'should not mention carthage' do
-    rendered_readme_for("AlamoFire").should.not contain('install Carthage')
+    rendered_readme_for("Alamofire").should.not contain('install Carthage')
   end
-  
+
   it 'should not feature CocoaPods' do
-    rendered_readme_for("AlamoFire").should.not contain("pod 'Alamo")
+    rendered_readme_for("Alamofire").should.not contain("pod 'Alamo")
   end
 end
 
@@ -73,28 +73,28 @@ describe 'with SwityJSON' do
   it 'should not mention carthage' do
     rendered_readme_for("SwiftyJSON").should.not contain('You can use Carthage')
   end
-  
+
   it 'should not feature CocoaPods' do
     rendered_readme_for("SwiftyJSON").should.not contain("pod 'Swifty")
   end
 end
 
 describe 'with Expecta' do
-  
+
   it 'should not feature CocoaPods' do
     rendered_readme_for("Cent").should.not contain("pod 'Expecta")
   end
-  
+
   it 'should mention carthage' do
     readme_html = rendered_readme_for("Expecta")
     readme_html.should contain('using Carthage')
   end
-  
+
   it 'should not link to the carthage repo' do
     readme_html = rendered_readme_for("Expecta")
     readme_html.should.not contain('github.com/Carthage/Carthage')
   end
-  
+
   it 'should remove larger carthage section' do
     readme_html = rendered_readme_for("Expecta")
     readme_html.should.not contain('carthage update')
