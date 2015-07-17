69f5596ce4e8725a97fcf7af64e3a69623806f6f
Orta Therox <orta.therox@gmail.com>
2015-07-17 12:13:28 +0100

    Update ping.rb to use nap

--- ping.rb
+++ ping.rb
@@ -17,29 +17,24 @@ def send(number, metric_id)
 end
 
 
-begin
-  number = REST.get("http://localhost:4567/recent_pods_count").response.body
-  puts "Sending #{number} pods to Status.io"
+number = REST.get("http://localhost:4567/recent_pods_count").response.body
+puts "Sending #{number} pods to Status.io"
 
-  # Send CocoaDocs stats
-  cd_metric_id = ENV['STATUS_IO_METRIC_ID']
-  send(number, cd_metric_id)
+# Send CocoaDocs stats
+cd_metric_id = ENV['STATUS_IO_METRIC_ID']
+send(number, cd_metric_id)
 
-  get_url = "http://stats.cocoapods.org/api/v1/recent_requests_count"
-  reset_url = "http://stats.cocoapods.org/api/v1/reset_requests_count"
+get_url = "http://stats.cocoapods.org/api/v1/recent_requests_count"
+reset_url = "http://stats.cocoapods.org/api/v1/reset_requests_count"
 
-  number = REST.get(get_url).response.body
-  puts "Sending #{number} of stats to Status.io"
+number = REST.get(get_url).response.body
+puts "Sending #{number} of stats to Status.io"
 
-  # Send stats.cocoapods.org stats
-  stats_metric_id = ENV['STATUS_IO_STATS_METRIC_ID']
-  send(number, stats_metric_id)
+# Send stats.cocoapods.org stats
+stats_metric_id = ENV['STATUS_IO_STATS_METRIC_ID']
+send(number, stats_metric_id)
 
-  # Reset stats.cocoapods.org stats
-  REST.post(reset_url)
+# Reset stats.cocoapods.org stats
+REST.post(reset_url)
 
-  sleep 1
-
-rescue Exception => e
-  puts "Exception: #{e}"
-end
+sleep 1
