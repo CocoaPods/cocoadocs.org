3465ba3e7b667b0a382c15179457fd37ff03a089
Orta Therox <orta.therox@gmail.com>
2015-08-08 09:18:15 -0400

    Add support for .cocoapods.yml files as well as cocoadocs.yml settings

--- classes/cocoadocs_settings.rb
+++ classes/cocoadocs_settings.rb
@@ -2,8 +2,9 @@ class CocoaDocsSettings
 
   def self.settings_at_location download_location
     cocoadocs_settings = download_location + "/.cocoadocs.yml"
+    cocoadocs_settings = download_location + "/.cocoapods.yml" unless File.exist? cocoadocs_settings
+    
     settings = YAML.load(File.read(Dir.pwd + "/views/cocoadocs.defaults.yml"))
-
 
     if File.exist? cocoadocs_settings
       vputs "- found custom CocoaDocs settings"
