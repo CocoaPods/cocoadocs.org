8e38dd18f49280d0762371f3db57b46889e37c74
Orta Therox <orta.therox@gmail.com>
2016-01-07 19:10:02 +0000

    [Jazzy] send in a Podspec instead of the url to Jazzy

--- cocoadocs.rb
+++ cocoadocs.rb
@@ -430,7 +430,7 @@ class CocoaDocs < Object
           c.config_file = Pathname(jazzy_config) if jazzy_config
           c.parse_config_file
 
-          c.podspec = Pathname(download_spec_path)
+          c.podspec = Pod::Specification.from_file(download_spec_path)
           c.output = Pathname(docset_location)
           c.min_acl = Jazzy::SourceDeclaration::AccessControlLevel.public
           c.docset_icon = Pathname(__FILE__).parent + 'resources/docset_icon.png'
@@ -439,8 +439,6 @@ class CocoaDocs < Object
           c.source_directory = Pathname(download_location)
           c.clean = true
           c.dash_url = "#{$website_home}docsets/#{spec.name}/#{spec.name}.xml"
-          c.module_name = spec.name
-          c.author_name = spec.or_contributors_to_spec
         end
 
         begin
