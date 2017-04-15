b323012b17c9e1bf24af02869d196db535b9f794
Orta Therox <orta.therox@gmail.com>
2017-04-15 11:54:16 -0400

    Make simple work

--- cocoadocs-simple.rb
+++ cocoadocs-simple.rb
@@ -3,29 +3,31 @@
 # Usage: cocoadocs-simple [podspec_url]
 # Will generate a README, CHANGELOG, and metrics for CocoaDocs
 
-require "cocoapods-downloader"
-require "cocoapods-core"
-require "cocoapods"
+require 'cocoapods-downloader'
+require 'cocoapods-core'
+require 'cocoapods'
 
-require "open-uri"
+require 'open-uri'
+
+current_dir = File.dirname(File.expand_path(__FILE__))
 
 # Ensure all the class files exist in scope
-Dir[File.join(current_dir, "classes/*.rb")].each do |file|
+Dir[File.join(current_dir, 'classes/*.rb')].each do |file|
   require_relative(file)
 end
 
-$specs_repo = "CocoaPods/Specs"
-$s3_bucket = "cocoadocs.org"
-$website_home = "http://cocoadocs.org/"
-$cocoadocs_specs_name = "cocoadocs_specs"
-
+$specs_repo = 'CocoaPods/Specs'
+$s3_bucket = 'cocoadocs.org'
+$website_home = 'http://cocoadocs.org/'
+$cocoadocs_specs_name = 'cocoadocs_specs'
+$active_folder = 'activity'
 
 def run
-  specs_repo = "CocoaPods/Specs"
-  s3_bucket = "cocoadocs.org"
-  website_home = "http://cocoadocs.org/"
+  specs_repo = 'CocoaPods/Specs'
+  s3_bucket = 'cocoadocs.org'
+  website_home = 'http://cocoadocs.org/'
 
-  active_folder_name = "activity"
+  active_folder_name = $active_folder
   current_dir = File.dirname(File.expand_path(__FILE__))
   active_folder = File.join(current_dir, active_folder_name)
 
@@ -34,26 +36,26 @@ def run
   spec = nil
 
   # Verify we're working with a CP Spec
-  unless url.starts_with? "https://raw.githubusercontent.com/CocoaPods/Specs"
-    puts "Not running non-CocoaPods Spec URL"
+  unless url.start_with? 'https://raw.githubusercontent.com/CocoaPods/Specs'
+    puts 'Not running non-CocoaPods Spec URL'
     return
   end
 
   # Setup a local copy
-  spec_name = url.split("/")[-1]
-  podspec_path = active_folder + "/podspecs/" + spec_name
+  spec_name = url.split('/')[-1]
+  podspec_path = active_folder + '/podspecs/' + spec_name
 
   # Put it in the filesystem
   FileUtils.mkdir_p(File.dirname(podspec_path))
-  open(url) { |f| File.open(podspec_path, "w") { |tmp| tmp.write(f.read) } }
+  open(url) { |f| File.open(podspec_path, 'w') { |tmp| tmp.write(f.read) } }
 
   # Eval it, and process
   spec = Pod::Specification.from_file(podspec_path)
 
   download_location  = active_folder + "/download/#{spec.name}/#{spec.version}/#{spec.name}"
   docset_location    = active_folder + "/docsets/#{spec.name}/#{spec.version}/"
-  readme_location    = active_folder + "/readme/#{spec.name}/#{spec.version}/index.html"
-  changelog_location = active_folder + "/changelog/#{spec.name}/#{spec.version}/index.html"
+  readme_location    = active_folder + "/readme/#{spec.name}/#{spec.version}/README.html"
+  changelog_location = active_folder + "/changelog/#{spec.name}/#{spec.version}/CHANGELOG.html"
   api_json_location  = active_folder + "/docsets/#{spec.name}/#{spec.version}/stats.json"
 
   # Download the source code
@@ -103,27 +105,27 @@ def run
 
   # Upload the files to S3
   generator = WebsiteGenerator.new(generate_json: false, spec: spec)
-  generator.upload_docset
 
-  # Make notes of the builds
-  HistoryLogger.new(spec: spec, download_location: docset_location, source_download_location: download_location).append_state state
+  server_folder = "docsets"
+  generator.upload_folder "/readme/#{spec.name}/#{spec.version}/README.html", "/#{server_folder}/#{spec.name}/", "put"
+  generator.upload_folder "/changelog/#{spec.name}/#{spec.version}/CHANGELOG.html", "/#{server_folder}/#{spec.name}/", "put"
 
   # Give a clickable link
-  puts "* - " + website_home + "docsets/" + spec.name + "/" + spec.version.to_s + "/"
+  puts '* - ' + website_home + 'docsets/' + spec.name + '/' + spec.version.to_s + '/'
 
 rescue StandardError => e
   log_error(spec, e) unless spec.nil?
 
-  open("error_log.txt", "a") do |f|
-    f.puts "\n\n\n --------------#{spec.defined_in_file}-------------"
+  open('error_log.txt', 'a') do |f|
+    f.puts "\n\n\n --------------#{spec}-------------"
     f.puts e.message
-    f.puts "------"
+    f.puts '------'
     f.puts e.backtrace.inspect
   end
 
-  puts "--------------#{spec.defined_in_file}-------------".red
+  puts "--------------#{spec}-------------".red
   puts e.message.red
-  puts "------"
+  puts '------'
   puts e.backtrace.inspect.red
 end
 
@@ -132,10 +134,10 @@ def log_error(spec, e)
   FileUtils.mkdir_p(File.dirname(error_path))
   FileUtils.rm(error_path) if File.exist? error_path
 
-  open(error_path, "a") do |f|
+  open(error_path, 'a') do |f|
     report = {
-      "message" => e.message.encode("utf-8", "binary", undef: :replace),
-      "trace" => e.backtrace
+      'message' => e.message.encode('utf-8', 'binary', undef: :replace),
+      'trace' => e.backtrace
     }
     f.puts report.to_json.to_s
   end
